46ce34ea038002de3dd476cd528c24bdb5dbd8e2
Newul <1015646539@qq.com>
2016-03-09 21:36:42 +0800

    66666

--- test/src/fuli.java
+++ test/src/fuli.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 /**
  * Created by Administrator on 2016/3/9.
  */
 public class fuli {
+public static void main(String args[]){
+    Scanner scanner =new Scanner (System.in );
+    double F,p,i,n;
+    System .out.printf("输入初始金额");
+    p=scanner.nextDouble() ;
+    System .out.printf("输入投资年限");
+    n=scanner.nextDouble() ;
+    System .out.printf("输入利率");
+    i=scanner.nextDouble() ;
+    F=p*Math .pow(1+i,n);
+    System .out.print(F);
 
 
+    }
 
 }
+
+
+
+
